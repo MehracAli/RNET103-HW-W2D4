01d2a7015ad660020a66f7c781f144f437fc8bf7
MehracAli <mehrajvm@code.edu.az>
2023-12-17 20:21:02 +0400

    hw done

---
+++ Login/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+    dışarıdan kullanıcı adı ve şifre alınacak, kullanıcı login olana dek bu işlemi tekrarlayacak
+
+    - verileri tupple'da tutunuz :) 
+    - (string username, string password) user = ("admin", "123");
+*/
+
+(string username, string password) user = new("admin".ToLower(),"admin123".ToLower());
+
+string username = string.Empty;
+string password = string.Empty;
+
+bool result = false;
+
+do
+{
+    Console.Write("Username: ");
+    username = Console.ReadLine();
+    Console.Write("Password: ");
+    password = Console.ReadLine();
+
+    result = (user.username == username && user.password == password)? result = true : result = false;
+
+    Console.WriteLine("Invalid username or password!");
+    
+
+} while (!result);
+
+Console.WriteLine($"{user} is logged in");
